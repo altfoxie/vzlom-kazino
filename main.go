8306f754aa75d3a203155a31360df71ff7e5d28d
Max <maxunof@pm.me>
2021-12-08 21:17:29 +0300

    better vzlom kazino

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
+	"github.com/wailsapp/wails/v2/pkg/options"
+)
+
+//go:embed frontend/src
+var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
+
+func main() {
+	app := NewApp()
+
+	err := wails.Run(&options.App{
+		Title:         "Взлом КАЗИНО",
+		Width:         402,
+		Height:        202,
+		DisableResize: true,
+		Frameless:     true,
+		Assets:        assets,
+		LogLevel:      logger.DEBUG,
+		OnStartup:     app.startup,
+		OnDomReady:    app.domReady,
+		StartHidden:   true,
+		Bind: []interface{}{
+			app,
+		},
+		Mac: &mac.Options{
+			About: &mac.AboutInfo{
+				Title:   "Взлом КАЗИНО",
+				Message: "взлом казино",
+				Icon:    icon,
+			},
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
